9f91af42cbde5ebe542f2eb0a557ec364f569e71
Sathish <sat.hariharan@gmail.com>
2021-11-22 21:51:09 +0530

    reset App.js

--- src/App.js
+++ src/App.js
@@ -25,35 +25,6 @@ import NavBar from './components/NavBar'
 import AddItemCard from './components/AddItemCard'
 import { Grid } from '@material-ui/core'
 
-// import Amplift and Hub
-import { Amplify, Hub } from 'aws-amplify';
-// Retrieve confirguation info from aws-exports and cdk-exports
-import config from './aws-exports';
-import { BackendStack } from "./cdk-exports.json"
-
-// import the Authenticator and ui for react
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-
-// setup configurations
-const cdkConfig = {
-  API: {
-    endpoints: [
-      {
-        name: BackendStack.apiname,
-        endpoint: BackendStack.apiendpoint
-      }
-    ]
-  }, 
-  aws_cognito_region: BackendStack.awscognitoregion,
-  aws_user_pools_id:  BackendStack.userpool,
-  aws_user_pools_web_client_id: BackendStack.webclientid,
-  aws_cognito_identity_pool_id: BackendStack.identitypool,
-}
-
-Amplify.configure(config);
-Amplify.configure(cdkConfig);
-
 function App() {
 
   const [items, setItems] = useState([])
@@ -67,21 +38,8 @@ function App() {
     setItems(await getUserItems())
   }
   
-  // use the Hub to remspond toe events
-  Hub.listen('auth', (data) => {
-    if (data.payload.event === 'signIn') {
-      fetchData()
-    } else if (data.payload.event === 'signOut') {
-      window.location.reload();
-    }
-  });
 
-  return (
-    // Wrap the code with authenticator, emnable email as an additional attribute
-    <Authenticator signUpAttributes={[
-      'email',
-    ]}>
-    {() => (  
+  return (  
     <div className="app">
       <NavBar/>
       
@@ -112,8 +70,6 @@ function App() {
         </Grid>
       </div>
     </div>
-    )}
-    </Authenticator>
   );
 }
 
